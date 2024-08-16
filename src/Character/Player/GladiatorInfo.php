b6ab15cf9231d50b944ffe35c0d40a458a4c9264
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-08-16 16:58:06 +0200

    Stats direkt aus den Klassenattributen auslesen

--- src/Character/Player/GladiatorInfo.php
+++ src/Character/Player/GladiatorInfo.php
@@ -11,9 +11,28 @@ class GladiatorInfo extends CharacterAtt
         parent::__construct("Gladiator",1,1,130,6,1.2,4,1.6,3,0.3,3,0.6,4,1.5);
     }
 
+    // TODO: warum gibst du hier nicht die bereits gesetzten Stats aus? (siehe Constructor)
     public function show($name,$exp,$lvl,$hp): string
     {
         return ($name.$exp.$lvl.$hp);
+    }
+
+    /**
+     * HINWEIS: Hier haben wir eine besondere Form der String-Formatierung, hatten wir noch nicht.
+     * Mehr dazu unter <a href="https://www.php.net/manual/de/language.types.string.php">diesem Link</a> (Heredoc-Syntax)
+     * @return string Statuswerte
+     */
+    public function showStats(): string
+    {
+        return
+            <<<STATS
+            Name: $this->name
+            Exp: $this->exp
+            Lvl: $this->lvl
+            HP: $this->hp
+            
+            STATS
+        ;
     }
 
 
